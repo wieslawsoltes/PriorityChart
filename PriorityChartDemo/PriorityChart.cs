0d9a7baea9b5dfc79cae54c56fadef02c83182cc
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2021-01-25 23:05:25 +0100

    Refactor

--- PriorityChartDemo/PriorityChart.cs
+++ PriorityChartDemo/PriorityChart.cs
@@ -55,12 +55,10 @@ namespace PriorityChartDemo
             var width = this.Bounds.Width;
             var height = this.Bounds.Height;
 
-
             var topMargin = 20.0;
             var bottomMargin = 60.0;
             var leftMargin = 60.0;
             var rightMargin = 20.0;
-
 
             var valuesWidth = width - leftMargin - rightMargin;
             var valuesHeight = height - topMargin - bottomMargin;
@@ -73,53 +71,51 @@ namespace PriorityChartDemo
                 points[i] = new Point(i * step, scaledValues[i]);
             }
 
-            
-            
-            var geometryValuesLine = new StreamGeometry();
-            using var geometryContextLine =  geometryValuesLine.Open();
-            geometryContextLine.BeginFigure(points[0], false);
+            DrawDataFill(context, points, valuesWidth, valuesHeight, leftMargin, topMargin);
+            DrawDataStroke(context, points, leftMargin, topMargin);
+            DrawLabels(context, step, height, rightMargin, topMargin, bottomMargin);
+            DrawBorder(context);
+        }
+
+        private void DrawDataFill(DrawingContext context, Point[] points, double width, double height, double leftMargin, double topMargin)
+        {
+            var geometry = new StreamGeometry();
+            using var geometryContext = geometry.Open();
+            geometryContext.BeginFigure(points[0], true);
             for (var i = 1; i < Values.Count; i++)
             {
-                geometryContextLine.LineTo(points[i]);
+                geometryContext.LineTo(points[i]);
             }
-            geometryContextLine.EndFigure(false);
-            var brushValuesLine = new ImmutableSolidColorBrush(Color.FromArgb(255, 66, 133, 244));
-            var penValuesLine = new ImmutablePen(brushValuesLine, 2);
-            var valuesLineTransform = context.PushPreTransform(Matrix.CreateTranslation(leftMargin, topMargin));
-            context.DrawGeometry(null, penValuesLine, geometryValuesLine);
-            valuesLineTransform.Dispose();
+            geometryContext.LineTo(new Point(width, height));
+            geometryContext.LineTo(new Point(0, height));
+            geometryContext.EndFigure(true);
+            var brush = new ImmutableSolidColorBrush(Color.FromArgb(255, 198, 218, 252));
+            var transform = context.PushPreTransform(Matrix.CreateTranslation(leftMargin, topMargin));
+            context.DrawGeometry(brush, null, geometry);
+            transform.Dispose();
+        }
 
-            
-
-            
-            var geometryValuesFill = new StreamGeometry();
-            using var geometryContextFill =  geometryValuesFill.Open();
-            geometryContextFill.BeginFigure(points[0], true);
+        private void DrawDataStroke(DrawingContext context, Point[] points, double leftMargin, double topMargin)
+        {
+            var geometry = new StreamGeometry();
+            using var geometryContext = geometry.Open();
+            geometryContext.BeginFigure(points[0], false);
             for (var i = 1; i < Values.Count; i++)
             {
-                geometryContextFill.LineTo(points[i]);
+                geometryContext.LineTo(points[i]);
             }
-            geometryContextFill.LineTo(new Point(valuesWidth, valuesHeight));
-            geometryContextFill.LineTo(new Point(0, valuesHeight));
-            geometryContextFill.EndFigure(true);
-            var brushValuesFill = new ImmutableSolidColorBrush(Color.FromArgb(255, 198, 218, 252));
-            var valuesFillTransform = context.PushPreTransform(Matrix.CreateTranslation(leftMargin, topMargin));
-            context.DrawGeometry(brushValuesFill, null, geometryValuesFill);
-            valuesFillTransform.Dispose();   
+            geometryContext.EndFigure(false);
+            var brush = new ImmutableSolidColorBrush(Color.FromArgb(255, 66, 133, 244));
+            var pen = new ImmutablePen(brush, 2);
+            var transform = context.PushPreTransform(Matrix.CreateTranslation(leftMargin, topMargin));
+            context.DrawGeometry(null, pen, geometry);
+            transform.Dispose();
+        }
 
-            
-            
-            var brushBorder = new ImmutableSolidColorBrush(Color.FromArgb(255, 0, 0, 0));
-            var penBorderThickness = 1.0;
-            var penBorder = new ImmutablePen(brushBorder, penBorderThickness);
-            var borderRect = new Rect(0, 0, this.Bounds.Width, this.Bounds.Height);
-            var borderRectDeflate = borderRect.Deflate(penBorderThickness * 0.5);
-            context.DrawRectangle(null, penBorder, borderRectDeflate, 0, 0);
-
-
-
-            var brushText = new ImmutableSolidColorBrush(Color.FromArgb(255, 0, 0, 0));
-            var typeface = new Typeface("Arial");
+        private void DrawLabels(DrawingContext context, double step, double height, double rightMargin, double topMargin, double bottomMargin)
+        {
+            var brush = new ImmutableSolidColorBrush(Color.FromArgb(255, 0, 0, 0));
+            var typeface = new Typeface("system", FontStyle.Normal, FontWeight.Normal);
             for (var i = 0; i < Labels.Count; i++)
             {
                 var origin = new Point(rightMargin + i * step, height - topMargin);
@@ -132,17 +128,23 @@ namespace PriorityChartDemo
                     FontSize = 12,
                     Constraint = new Size(step, bottomMargin)
                 };
-                var matrix = Matrix.CreateTranslation(-origin.X, -origin.Y) 
+                var matrix = Matrix.CreateTranslation(-origin.X, -origin.Y)
                              * Matrix.CreateRotation(-Math.PI / 4)
                              * Matrix.CreateTranslation(origin.X, origin.Y);
-                var textTransform = context.PushPreTransform(matrix);
-                context.DrawText(brushText, origin, formattedText);
-                textTransform.Dispose();
+                var transform = context.PushPreTransform(matrix);
+                context.DrawText(brush, origin, formattedText);
+                transform.Dispose();
             }
+        }
 
-            
-            
-            
+        private void DrawBorder(DrawingContext context)
+        {
+            var brush = new ImmutableSolidColorBrush(Color.FromArgb(255, 0, 0, 0));
+            var thickness = 1.0;
+            var pen = new ImmutablePen(brush, thickness);
+            var rect = new Rect(0, 0, this.Bounds.Width, this.Bounds.Height);
+            var rectDeflate = rect.Deflate(thickness * 0.5);
+            context.DrawRectangle(null, pen, rectDeflate, 0, 0);
         }
     }
 }
